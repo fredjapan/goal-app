0f7198c0e317fb78b2eae32ba8b0ba67b3983a69
frederic peyrot <frederic.peyrot@gmail.com>
2020-04-10 16:41:06 +0900

    Auto check this/next week radio in edit Hide date in edit for previous goals

--- app/models/weeklygoal.rb
+++ app/models/weeklygoal.rb
@@ -7,4 +7,13 @@ class Weeklygoal < ActiveRecord::Base
     i = self.date.strftime('%Y%W')
     Date.commercial(i[0,4].to_i,i[4,2].to_i+1)
   end
+
+  def this_week?
+    date >= DateTime.current.to_date.beginning_of_week && date <= DateTime.current.to_date.end_of_week
+  end
+
+  def next_week?
+    date >= 1.week.from_now.to_date.beginning_of_week && date <= 1.week.from_now.to_date.end_of_week
+  end
+
 end
