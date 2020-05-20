9bc506ce3cd761ba2b3a682e789b4918ea8dbf9f
frederic peyrot <frederic.peyrot@gmail.com>
2020-05-20 16:50:08 +0900

    fixed bulma extension import index views display properly with horizon

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,7 +1,13 @@
 module GoalsHelper
 
-  def beginning_of_term(date)
-    date.beginning_of_week
+  def next_term(horizon)
+    if horizon == "week"
+      1.week
+    elsif horizon == "quarter"
+      3.months
+    elsif horizon == "year"
+      1.year
+    end
   end
 
 end
