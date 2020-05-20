68a757445388265a59410b29d0f07bad8503164a
frederic peyrot <frederic.peyrot@gmail.com>
2020-05-20 23:00:22 +0900

    added achievement percentages to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,22 @@ class PagesController < ApplicationController
     @goal_quarter_count = Goal.where(horizon: "quarter").count
     @goal_year_count = Goal.where(horizon: "year").count
     @goal_life_count = Goal.where(horizon: "life").count
-    @goal_all_count = @goal_week_count + @goal_quarter_count + @goal_year_count + @goal_life_count
-    @goal_week_achieved_count = Goal.where(horizon: "week", achievement: "achieved")
+
+    def achievment_percentage(horizon, achievement)
+      total_goal_withachievment = Goal.where(horizon: horizon).where.not(achievement: [nil, ""]).count
+      goal_achievement_count = Goal.where(horizon: horizon, achievement: achievement).count
+      goal_achievement_count.to_f / total_goal_withachievment.to_f
+    end
+    
+    @goal_week_achieved_percentage = achievment_percentage("week", "achieved")
+    @goal_week_partiallyachieved_percentage = achievment_percentage("week", "partially achieved")
+    @goal_week_failed_percentage = achievment_percentage("week", "failed")
+    @goal_quarter_achieved_percentage = achievment_percentage("quarter", "achieved")
+    @goal_quarter_partiallyachieved_percentage = achievment_percentage("quarter", "partially achieved")
+    @goal_quarter_failed_percentage = achievment_percentage("quarter", "failed")
+    @goal_year_achieved_percentage = achievment_percentage("year", "achieved")
+    @goal_year_partiallyachieved_percentage = achievment_percentage("year", "partially achieved")
+    @goal_year_failed_percentage = achievment_percentage("year", "failed")
   end
 
 end
