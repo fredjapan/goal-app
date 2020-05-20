43b43dc2234f1452fb30a4fbe3a59c541a9596e0
frederic peyrot <frederic.peyrot@gmail.com>
2020-05-20 22:23:40 +0900

    Fixed achievement capitalization

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,12 @@
 class PagesController < ApplicationController
+  
+  def home
+    @goal_week_count = Goal.where(horizon: "week").count
+    @goal_quarter_count = Goal.where(horizon: "quarter").count
+    @goal_year_count = Goal.where(horizon: "year").count
+    @goal_life_count = Goal.where(horizon: "life").count
+    @goal_all_count = @goal_week_count + @goal_quarter_count + @goal_year_count + @goal_life_count
+    @goal_week_achieved_count = Goal.where(horizon: "week", achievement: "achieved")
+  end
 
 end
