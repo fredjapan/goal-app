083320b8befeb233806fcee0724b5e075bf620e9
frederic peyrot <frederic.peyrot@gmail.com>
2020-06-10 22:36:39 +0900

    added PW confirmation and min length

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(params.require(:user).permit(:email, :password))
+    @user = User.create(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
     else
       render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 end
