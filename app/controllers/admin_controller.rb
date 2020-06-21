460584fabffefff61f453e0c869fac794e6a3310
frederic peyrot <frederic.peyrot@gmail.com>
2020-06-21 23:44:58 +0900

    fixed admin sorting by goals

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,9 @@ class AdminController < ApplicationController
   helper_method :sort_column, :sort_direction
   def index
     @users = User.order(sort_column + " " + sort_direction)
+    @users = @users.map{|user| [user, user.goals.count + user.life_goals.count]}
+    @users = @users.sort_by{|array| array[1]} if params[:sort_by_goal] == "asc"
+    @users = @users.sort_by{|array| array[1]}.reverse if params[:sort_by_goal] == "desc"
   end
 
   private
