e963a4ecd0433792e260744e1eac2f7d273b1d31
frederic peyrot <frederic.peyrot@gmail.com>
2020-06-30 22:43:46 +0900

    remove pw length from validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
 
   validates :email, presence: {message: "must be present."}
   validates :email, uniqueness: {message: "already exists."}
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
-  validates :password, length: { minimum: 6, too_short: "must be at least 6 character long."}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   has_secure_password
   
